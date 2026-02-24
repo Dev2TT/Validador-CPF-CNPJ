d911e91f5fb86d9faef998bd7b9315e0ede09c7f
Dev2TT <victtor.viltrumita@gmail.com>
2026-02-23 21:26:30 -0300

    Definindo configuracoes iniciais do projeto

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from config import Config
+from extensions import db
+
+def create_app():
+    app=Flask(__name__)
+    app.config.from_object(Config)
+
+    try:
+        db.init_app(app)
+    except Exception as e:
+        print(f'Nao foi possível inicializar o banco de dados {str(e.args)}')
+
+
+    return app
